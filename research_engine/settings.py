56877dddbb213f99fb8cb237f8ab33aea0eefd33
Arseniy Shapiro <arseniy.shapiro@gmail.com>
2024-02-20 18:55:32 +0400

    deleted garbage, renamed media_root and started db from scratch

--- research_engine/settings.py
+++ research_engine/settings.py
@@ -129,7 +129,7 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 
 STATIC_URL = 'static/'
 
-MEDIA_ROOT = 'uploads/'
+MEDIA_ROOT = 'media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
